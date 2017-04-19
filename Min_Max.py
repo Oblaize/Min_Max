bb3127da0e2968b36142c6bed2d1c4329269cdf6
Blaize Ottizy <Blaize Ottizy>
2017-04-19 13:35:00 +0300

    minimum and maximum number

---
+++ Min_Max.py
@@ -0,0 +1,17 @@
+def find_max_min(data):
+    """find minimum and maximum values in a container
+    
+    args:
+        data (list): list or iterable
+        
+    returns:
+        list: [min_val, max_val] or [min_val or max_val]
+    """
+    # use internal functions to get min and max values
+    min_max = [min(data), max(data)]
+    
+    # check if min and max values are equal
+    if min(data) == max(data):
+        return [min(data)]
+    else:
+        return min_max
